732cf596807a71ffb758679d2e3c289beeafb8dd
Brandon Nolan <brandon.nolan@stratahealth.com>
2020-02-17 14:44:22 -0700

    enum setting back

--- enums/Extension.py
+++ enums/Extension.py
@@ -1,7 +1,7 @@
 from aenum import Enum, NoAlias
 
 
-class Extension(Enum):
+class Extension(Enum, settings=NoAlias):
     """
         File extensions handled and there associated folder names.
     """
